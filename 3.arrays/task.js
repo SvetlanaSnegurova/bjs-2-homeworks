9396095cca9a8bccc40ec29f94152361a5f0d2cd
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-07-16 00:30:36 +0300

    added functions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
+'use strict'
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter((user) => user.gender === gender)
+           .map((user) => user.age)
+           .reduce((acc, age, item, arr) => acc + age / arr.length, 0) 
+        
 }
