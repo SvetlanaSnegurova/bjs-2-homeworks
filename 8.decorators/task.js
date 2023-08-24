2f91b7d7a44fd3910fc2a428af1269e7f672e5f1
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-08-24 23:36:53 +0300

    added decorator

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
-//Задача № 1
+"use strict"
+
 function cachingDecoratorNew(func) {
-  
+    const cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args);
+
+        const objectInCache = cache.find((item) => item.hash === hash);
+        if (objectInCache) {
+            return "Из кеша: " + objectInCache.value;
+        } else {
+            const value = func(...args);
+            cache.push({hash, value});
+
+            if (cache.length > 5) {
+                cache.shift();
+            }
+            return "Вычисляем: " + value;
+        }
+    }
+    return wrapper;
 }
 
-//Задача № 2
+// Задача №2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+    let isTrottled = false;
+
+    function wrapper(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func(args);
+            wrapper.count++;
+        }, delay);
+        if (!isTrottled) {
+            func(...args);
+            wrapper.count++;
+            isTrottled = true;
+        }
+        wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
